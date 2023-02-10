245d9a060d156ec8a043c82acdccd9dfeb8716db
Ryan Taylor <ryan.taylor1@yahoo.com>
2023-02-09 16:03:51 -0800

    set up models for user, thought, reaction

---
+++ models/thought.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require("mongoose");
+const reactionSchema = require("./reaction");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      min_length: 1,
+      max_length: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      get: (date) => {
+        if (date) return date.toISOString().split("T")[0];
+      },
+    },
+    username: { type: String, required: true },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual("reactionCount")
+.get(function(){
+    return this.reactions.length
+})
+
+module.exports = thoughtSchema;
