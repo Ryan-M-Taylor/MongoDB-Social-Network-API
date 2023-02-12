395faaa01cc24d826e367352b56b2cb1c6873423
Ryan Taylor <ryan.taylor1@yahoo.com>
2023-02-11 21:01:45 -0800

    added code for routes and controllers

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  deleteUser,
+  addThought,
+  removeThought,
+} = require('../../controllers/userController');
+
+// /api/students
+router.route('/').get(getUsers).post(createUser);
+
+// /api/students/:studentId
+router.route('/:userId').get(getSingleUser).delete(deleteUser);
+
+// /api/students/:studentId/assignments
+router.route('/:userId/reactions').post(addThought);
+
+// /api/students/:studentId/assignments/:assignmentId
+router.route('/:userId/reactions/:reactionId').delete(removeThought);
+
+module.exports = router;
